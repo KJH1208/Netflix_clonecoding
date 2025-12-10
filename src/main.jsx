7ad9bc30c8db080a0fce323209b3223fa13a119e
KJH1208 <guswnrkd12@gmail.com>
2025-12-10 15:06:27 +0900

    Add BrowserRouter basename for Github pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
