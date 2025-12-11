f7a70fdf90d15c171b61d35707ef019fb9764564
KJH1208 <guswnrkd12@gmail.com>
2025-12-11 21:04:39 +0900

    Router Basename추가

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // GitHub 저장소 이름
+  base: '/Netflix_clonecoding/',  // GitHub 저장소 이름
 })
